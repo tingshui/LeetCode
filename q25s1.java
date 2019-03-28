d7e2d8a5b2923aaee72078801de1c6ec87be0187
Qianying Lin <cug.angel.888@gmail.com>
2019-03-28 14:32:20 -0500

    Update q25s1.java

--- q25s1.java
+++ q25s1.java
@@ -8,43 +8,52 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k <= 0){
+        if (head == null || k == 1){
             return head;
         }
         int count = 0;
+        int t_count = 0;
         ListNode dummy = new ListNode(-1);
-        ListNode prev = new ListNode(-1);
-        ListNode next = null;
-        while (head!= null){
+        dummy.next = head;
+        ListNode prev = head;
+        ListNode movehead = head;
+        ListNode preend = head;
+        while (movehead!= null){
             if (count == k - 1){
+                ListNode movenext = movehead.next;
                 count = 0;
-                ListNode cnext = head.next;
-                List<ListNode> result = reverse(ListNode head, int k);
-                prev.next = result.get(1);
-                prev = result.get(0);
-                result.get(0).next = cnext;
+                ListNode newh = reverse(prev, k);
+                if (t_count == 0){
+                    dummy.next = newh;
+                    t_count++;
+                }
+                else{
+                    preend.next = newh;
+                    preend = prev;
+                }   
+                prev = movenext;
+                movehead = movenext;
             } 
             else{
                 count++;
-                head = head.next;
+                movehead = movehead.next;
             }
         }
+        return dummy.next;
     }
-    // reverse the list, return: end of reverselist, head of reverselist 
-    public List<ListNode> reverse(ListNode head, int k){
+    // reverse the list, and connect reversed list to original next node, return the head of reversed list
+    public ListNode reverse(ListNode head, int k){
+        ListNode dummy = head;
         ListNode prev = null;
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
+        ListNode cnext = null;
         while (k > 0){
-            ListNode cnext = head.next;
+            cnext = head.next;
             head.next = prev;
             prev = head;
             head = cnext;
             k--;
         }
-        List<ListNode> r = new ArrayList<ListNode>;
-        r.add(dummy);
-        r.add(prev);
-        return r;        
+        dummy.next = head;
+        return prev;        
     }
 }
