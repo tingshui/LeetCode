940bab27603cf6bdeed48e7dab64de549ef9dc22
Qianying Lin <cug.angel.888@gmail.com>
2019-04-08 23:12:59 -0500

    Create mergesort.java

---
+++ Review/mergesort.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class mergesort {
+	public static void main(String[] args) {
+		int[] nums = {-1,1,0};
+		int[] sorted = mergesort(nums);
+		System.out.print(Arrays.toString(sorted));
+	}
+
+	private static int[] mergesort(int[] nums){
+		int start = 0;
+		int end =  nums.length - 1;
+		if (start == end){
+			return nums;
+		}
+		int mid = (end - start)/2;
+		int[] left = Arrays.copyOfRange(nums, start, mid + 1);
+		int[] right = Arrays.copyOfRange(nums, mid + 1, end + 1);
+		mergesort(left);
+		mergesort(right);
+		merge(nums, left, right);
+		return nums;
+	}
+
+	private static void merge(int[] nums, int[] left, int[] right){
+		int num_p = 0;
+		int left_p = 0;
+		int right_p = 0;
+		while(num_p < nums.length){
+			if (left_p >= left.length){
+				nums[num_p] = right[right_p];
+				right_p++;
+				num_p++;
+				continue;
+			}
+			if (right_p >= right.length){
+				nums[num_p] = left[left_p];
+				left_p++;
+				num_p++;
+				continue;
+			}
+			if (left[left_p] <= right[right_p]){
+				nums[num_p] = left[left_p];
+				left_p++;
+				num_p++;
+				continue;
+			}
+			else{
+				nums[num_p] = right[right_p];
+				right_p++;
+				num_p++;
+			}
+		}
+	}
+}
