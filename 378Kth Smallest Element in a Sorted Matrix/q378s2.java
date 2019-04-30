25cdead8161afbb52f2b00d6251c72c4efbb551c
Qianying Lin <cug.angel.888@gmail.com>
2019-04-30 11:53:42 -0500

    Update q378s2.java

--- 378Kth Smallest Element in a Sorted Matrix/q378s2.java
+++ 378Kth Smallest Element in a Sorted Matrix/q378s2.java
@@ -4,4 +4,4 @@
 //           每一行具体COUNT多少个小于MID， 用[mid of row]去逼近MID在这一行的位置。
 //           n row, m col, 所以运行N次，每一次M个数字，所以总运行次数是 nlogm
 // 总复杂度为O(nlogm *  logX), m=n, 所以O(nlogn *  logX)
-// 本题是返回数字本身，所以直接用数字算MID就行。区别于求POSITION，他们要用POSITION来算
+// 本题是返回数字本身，所以直接用数字算MID就行。区别于求POSITION，他们要用POSITION来算，posistion本身就代表了有几个小于它。
