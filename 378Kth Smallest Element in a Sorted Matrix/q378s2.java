805648ae4a784c32e1ea78ddb9bbd93ab95e82be
Qianying Lin <cug.angel.888@gmail.com>
2019-04-30 16:07:36 -0500

    Update q378s2.java

--- 378Kth Smallest Element in a Sorted Matrix/q378s2.java
+++ 378Kth Smallest Element in a Sorted Matrix/q378s2.java
@@ -8,7 +8,7 @@
 
 // 我错的地方：我用COUNT(N) == K-1来结果。但是可能这个值不是在数列中。
 // 例如：{{1，4}，{2，5}}， k=3，我返回的结果是3，确实有两个数比3小，但是3不在数列中。
-// 所以不能把count=k-1作为判定唯一标准。
+// 所以不能把count=k-1作为判定唯一标准。找到一个类似count(start)<k-1, count(end)<=k-1,之类的值。
 
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
@@ -18,18 +18,22 @@ class Solution {
         int n = matrix.length;
         int start = matrix[0][0];
         int end = matrix[n-1][n-1];
-        while(start < end){
+        while(start + 1 < end){
             int mid =  start + (end-start)/2;
-            int count = countless(mid, matrix);
-            if(count == k){
-                return mid;
-            }
-            else if (count < k){
+            int count = countless(mid, matrix);   
+            // 这里count=k-1不能RETURN。因为可能MID这个数字不存在。所以要用start和end来缩小范围。
+            if (count <= k-1){
                 start = mid;
             }
             else{
                 end = mid;
             }
+        }
+        // if count(start)<k-1, count(end)>k-1, 说明有好几个START重复，{1，2，2，4}，k=3, c(2)=1,c(3)=3. get 2
+        // if count(start)<k-1, count(end)=k-1, {1,1,2,4}, k=3, c(2)=2, c(3) =3, get 2.
+        // if count(start)<k-1, count(end)<k-1, 说明有好几个end重复, {1,2,3,3}, k=3, c(2)=1, c(3) =2, get 3.
+        if (countless(end, matrix) <= k - 1){
+        	return end;
         }
         return start;
     }
@@ -38,29 +42,31 @@ class Solution {
         int count = 0;
         for (int i = 0; i < n; i++){
             int[] row = matrix[i];
-            int start = 0;
-            int end = row.length - 1;
-            while (start + 1 < end){
-                int lmid = start + (end - start)/2;
-                if (row[lmid] == mid){
-                    count += lmid;
-                    break;
-                }
-                else if (row[lmid] > mid){
-                    end = lmid;
-                }
-                else{
-                    start = lmid;
-                }
-            }
-            if (end < mid){
-                count += end;
-                continue;
-            }
-            if (start < mid){
-                count += start;
-            }            
+            count += binarysearch(row, mid);
         }
         return count;
+    }
+    public int binarysearch(int[] row, int mid){
+        int start = 0;
+        int end = row.length - 1;
+        while (start + 1 < end){
+            int lmid = start + (end - start)/2;
+            if (row[lmid] == mid){
+            	return lmid;
+            }
+            else if (row[lmid] > mid){
+                end = lmid;
+            }
+            else{
+                start = lmid;
+            }
+        }
+        if (row[end] < mid){
+        	return end + 1;
+        }
+        if (row[start] < mid){
+        	return start + 1;
+        }
+        return start;
     }
 }
