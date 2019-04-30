5fd525f5edaf1aeae91162ec59bdffef911ea273
Qianying Lin <cug.angel.888@gmail.com>
2019-04-30 11:45:30 -0500

    Update q378s2.java

--- 378Kth Smallest Element in a Sorted Matrix/q378s2.java
+++ 378Kth Smallest Element in a Sorted Matrix/q378s2.java
@@ -1,5 +1,5 @@
 // 用两个二分法来做：
-// outer二分（quick select）: select the k th element, 用 MID 值逼近。一共lg(max-min)次。
+// outer二分（quick select）: select the k th element, 用 count(MID) 值逼近k。一共lg(max-min)次。
 // inner二分：for n row:
 //           每一行具体COUNT多少个小于MID， 用[mid of row]去逼近MID在这一行的位置。
 //           n row, m col, 所以运行N次，每一次M个数字，所以总运行次数是 nlogm
