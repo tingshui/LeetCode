f70d4cc941616c5b278aa5f633b903f43860c527
Qianying Lin <cug.angel.888@gmail.com>
2019-05-03 14:52:27 -0500

    Update q209s1.java

--- 209Minimum Size Subarray Sum/q209s1.java
+++ 209Minimum Size Subarray Sum/q209s1.java
@@ -7,11 +7,10 @@ class Solution {
             return 0;
         }
         int left = 0;
-        int right = 0;
         int len = nums.length;
         int min = Integer.MAX_VALUE;
         int sum = 0;
-        while(right < len){
+        for (int right = 0; right < len; right++){
             sum += nums[right];
             // 这个地方要注意。不能用if,要用while。否则不能穷尽所有解。体会一下。
             //  如果是if，那么[1,2,4,3,0] 结果一定会包括0。
@@ -19,8 +18,7 @@ class Solution {
                 min = Math.min(min, right - left + 1);
                 sum -= nums[left];
                 left++;
-            }
-            right++;          
+            }      
         }
         // 这个地方要注意没有解的情况。
         return min == Integer.MAX_VALUE? 0 : min;
