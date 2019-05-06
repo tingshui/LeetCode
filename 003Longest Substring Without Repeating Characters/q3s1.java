c24e9fcc6cbcaca7497c41be62a9019ed2e956b5
Qianying Lin <cug.angel.888@gmail.com>
2019-05-05 21:55:33 -0500

    Update q3s1.java

--- 003Longest Substring Without Repeating Characters/q3s1.java
+++ 003Longest Substring Without Repeating Characters/q3s1.java
@@ -17,3 +17,23 @@ class Solution {
         return max;
     }
 }
+
+// 优化memory，可以用数组
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[256]; // map from character's ASCII to its last occured index        
+        int j = 0;
+        int i = 0;
+        int ans = 0;
+        for (i = 0; i < s.length(); i++) {
+            while (j < s.length() && map[s.charAt(j)]==0) {
+                map[s.charAt(j)] = 1;
+                ans = Math.max(ans, j-i + 1);
+                j ++;
+            }
+            map[s.charAt(i)] = 0;
+        }
+        
+        return ans;
+    }
+}
