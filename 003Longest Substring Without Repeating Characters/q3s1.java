059531c1ec78526f188d68eb9d864bff6130fa0b
Qianying Lin <cug.angel.888@gmail.com>
2019-05-05 21:09:38 -0500

    Create q3s1.java

---
+++ 003Longest Substring Without Repeating Characters/q3s1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int start = 0;
+        // 这里要用character,不能用string,否则后面containskey不认得相同的char
+        HashMap<Character, Integer> hmap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++){
+            // 错了一下，start在重复数字之后就不要更新。但是相等或者之前要更新。
+            if (hmap.containsKey(s.charAt(i)) && start <= hmap.get(s.charAt(i))){
+                start = hmap.get(s.charAt(i)) + 1;
+            }
+            hmap.put(s.charAt(i), i);            
+            max = Math.max(max, i - start + 1);
+        }
+        return max;
+    }
+}
