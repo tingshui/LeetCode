34c85ccac545831bcd661f25652fbad305ee7cc1
Qianying Lin <cug.angel.888@gmail.com>
2019-05-08 17:49:31 -0500

    Update q373s1.java

--- 373Find K Pairs with Smallest Sums/q373s1.java
+++ 373Find K Pairs with Smallest Sums/q373s1.java
@@ -1,7 +1,9 @@
 // 我做的是LINTCODE的版本。LEETCODE的输出结果有点麻烦，懒得搞。
+// 注意，这里重要的优化是设置了 priorityqueue的size为k。这影响到了时间复杂度的分析。
 // 这里的Visited借鉴了别人的String写法。也是挺新奇的。看看还有没有更好的方式。
-//每一次poll()需要logn，这里的n指的是pq里的元素个数，我们直到所有的sum个数只能是min(m,n) 
-//同时pq的size limit是k, 所以我们每一次poll()需要log min(m,n,k), for循环k次，所以时间复杂度是O(k.log(min(m,n,k))
+// 九章用的是boolean.更方便点。
+// 每一次poll()需要logn，这里的n指的是pq里的元素个数，pq的size limit是k, O(klogk）。
+// 但是 m 和 n 都有可能小于k，所以时间复杂度是O(k.log(min(m,n,k))
 public class Solution {
     public class Pair implements Comparable<Pair>{
         int aloc, bloc, sum;
@@ -21,8 +23,12 @@ public class Solution {
         if (A == null || B == null || k == 0 || A.length*B.length < k){
             return 0;
         }
+        // 注意这里要设置priorityqueue的size为k
+        //PriorityQueue<Pair> minHeap = new PriorityQueue<Pair>(k, new PairComparator());
         PriorityQueue<Pair> pq = new PriorityQueue<>();
         pq.offer(new Pair(0,0,A[0]+B[0]));
+        // 可以用数组代替这个hashset
+        //boolean[][] hash = new boolean[A.length][B.length];
         HashSet<String> visited = new HashSet<>();
         visited.add("0,0");
         for (int count = 0; count < k - 1; count++){
