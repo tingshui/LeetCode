aef555747d2c9ade7f98ece234946eb74744755a
Qianying Lin <cug.angel.888@gmail.com>
2019-05-13 22:30:11 -0500

    Update q721s1.java

--- 721Accounts Merge/q721s1.java
+++ 721Accounts Merge/q721s1.java
@@ -33,47 +33,76 @@ class Solution {
                     }
                     else{
                         root[findroot(root,emailSet.get(email.get(j)))] = findroot(root,emailSet.get(email.get(1)));
-                    }                    
+                    } 
+                    // 下面也是别人写的，处理这种情况。后面和前面的保持一致就可以了。其实和第一个一样是同一回事。
+//                     int pre_id = map.get(email);
+//                     int cur_id = i;
+//                     int pre_parent = findParent(parents, pre_id);
+//                     int cur_parent = findParent(parents, cur_id);
+//                     if(pre_parent != cur_parent){
+//                         parents[cur_parent] = pre_parent;
+//                     }
                 }
                 else{
                     emailSet.put(email.get(j), i);
                 }
             }
         }
-        
-        HashSet<String> added = new HashSet<>();
-        // first the group number, second is the location in result set
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        // i rep user i account
+        // 学习一下别人怎么写的
+        // 由于email有重复， 所以我们用 HashSet<String> 比ArrayList更好
+        Map<Integer,Set<String>> mergedMap = new HashMap();
         for (int i = 0; i < root.length; i++){
-            // nlist rep which items from the list have not yet been added.
-            List<String> nlist = new ArrayList<String>();
+            int rid = findroot(root, i);
+            if (!mergedMap.containsKey(rid)){
+                mergedMap.put(rid, new HashSet());
+            }
             for (int j = 1; j < accounts.get(i).size(); j++){
-                if(!added.contains(accounts.get(i).get(j))){
-                    added.add(accounts.get(i).get(j));
-                    nlist.add(accounts.get(i).get(j));
-                }
+                mergedMap.get(rid).add(accounts.get(i).get(j));
             }
-            if (hm.containsKey(findroot(root,root[i]))){
-                for (int j = 0; j < nlist.size(); j++){
-                    //修改2：这里不能用root[i],必须要找到最初始的root了。
-                    result.get(hm.get(findroot(root,root[i]))).add(nlist.get(j));                       
-                }                
-            }
-            else{
-                hm.put(findroot(root,root[i]), result.size());
-                List<String> newaccount = new ArrayList<String>();
-                newaccount.add(accounts.get(findroot(root,root[i])).get(0));
-                for (int j = 0; j < nlist.size(); j++){                    
-                    newaccount.add(nlist.get(j));                       
-                }
-                result.add(newaccount);
-            }
-        }       
-        for (int i = 0; i < result.size(); i++){
-            Collections.sort(result.get(i));
-        } 
+        }
+        for (Integer id: mergedMap.keySet()){
+            result.add(new ArrayList());
+            result.get(result.size() - 1).add(accounts.get(id).get(0));
+            List<String> emails = new ArrayList(mergedMap.get(id));
+            Collections.sort(emails);
+            result.get(result.size() - 1).addAll(emails);
+            
+        }
         return result;
+
+//         HashSet<String> added = new HashSet<>();
+//         // first the group number, second is the location in result set
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//         // i rep user i account
+//         for (int i = 0; i < root.length; i++){
+//             // nlist rep which items from the list have not yet been added.
+//             List<String> nlist = new ArrayList<String>();
+//             for (int j = 1; j < accounts.get(i).size(); j++){
+//                 if(!added.contains(accounts.get(i).get(j))){
+//                     added.add(accounts.get(i).get(j));
+//                     nlist.add(accounts.get(i).get(j));
+//                 }
+//             }
+//             if (hm.containsKey(findroot(root,root[i]))){
+//                 for (int j = 0; j < nlist.size(); j++){
+//                     //修改2：这里不能用root[i],必须要找到最初始的root了。
+//                     result.get(hm.get(findroot(root,root[i]))).add(nlist.get(j));                       
+//                 }                
+//             }
+//             else{
+//                 hm.put(findroot(root,root[i]), result.size());
+//                 List<String> newaccount = new ArrayList<String>();
+//                 newaccount.add(accounts.get(findroot(root,root[i])).get(0));
+//                 for (int j = 0; j < nlist.size(); j++){                    
+//                     newaccount.add(nlist.get(j));                       
+//                 }
+//                 result.add(newaccount);
+//             }
+//         }       
+//         for (int i = 0; i < result.size(); i++){
+//             Collections.sort(result.get(i));
+//         } 
+//         return result;
     }
     public int findroot(int[] root, int index){
         // improvement:可以直接这么写。好简洁！！！！
